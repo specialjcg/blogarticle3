ddd4d4ba81b5f4e99fecc37718561e7a4013eae5
Unknown <jc.gouleau@gmail.com>
2019-03-15 13:27:43 +0100

    blog

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Citation v-if="showComponent" @close="showComponent = false" />
+    <button @click="showComponent = true">
+      {{ title }}
+    </button>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import { Component, Vue, Prop } from "vue-property-decorator";
+import Citation from "./components/Citation.vue";
 
 @Component({
   components: {
-    HelloWorld
+    Citation
   }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  showComponent: Boolean = false;
+  title: string = "bonjour";
+}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: "Lucida Console", Monaco, monospace;
+
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+button {
+  color: #fbc831;
+  font-family: "Lucida Console", Monaco, monospace;
+  text-shadow: 3px 3px 3px #d17b0f;
+  background: rgba(0, 113, 0, 1.2);
+  background-repeat: no-repeat;
+  border: none;
+  cursor: pointer;
+  overflow: hidden;
+  outline: none;
+  border-radius: 5px;
+  text-decoration: none;
+  height: auto;
+  font-size: 2em;
+  font-weight: 800;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: auto;
+  margin-left: auto;
+  margin-right: auto;
+  transition: all 0.3s ease-out;
+}
+button:hover {
+  /*background:#4BA62A;*/
+  background-image: linear-gradient(
+    to left,
+    #007100,
+    #1b7e0c,
+    #2d8b17,
+    #3c9821,
+    #4ba62a
+  );
+  transition: all 0.3s ease-out;
+  transform-origin: 0% 50%;
+  transform: rotateY(6deg) rotateZ(1deg);
 }
 </style>
---
+++ src/components/Citation.vue
@@ -0,0 +1,77 @@
+<template>
+  <transition name="fade" @after-enter="showContent = true">
+    <div class="fond" @click="showContent = false">
+      <transition name="fade" @after-leave="$emit('close')">
+        <div v-if="showContent" class="content" @click.stop>
+          {{ Alea }}
+        </div>
+      </transition>
+    </div>
+  </transition>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class Citation extends Vue {
+  Alea: String = "";
+  showContent: boolean = false;
+  numbre: number = 0;
+  citationtext: Array<string> = [
+    "Exige beaucoup de toi-même et attends peu des autres. Ainsi beaucoup d'ennuis te seront épargnés.Confucius",
+    "Dans la vie on ne fait pas ce que l'on veut mais on est responsable de ce que l'on est.Jean Paul Sartre",
+    "La vie est un mystère qu'il faut vivre, et non un problème à résoudre.Gandhi",
+    "On a deux vies. La deuxième commence quand on réalise qu’on n’en a qu’une. Confucius",
+    "« Si vous pouvez le rêver, vous pouvez le faire. » Walt Disney",
+    "Si vous pensez que l’aventure est dangereuse, essayez la routine, elle est mortelle. » Paulo Coelho",
+    "« Un voyage de mille lieues commence toujours par un premier pas. » Lao Tseu",
+    "« J’ai des questions à toutes vos réponses »  Woody Allen ",
+    "« Les autres parlent, moi je travaille »picasso"
+  ];
+  mounted() {
+    this.Alea = this.citationtext[Math.floor(Math.random() * 9)];
+    window.setInterval(() => {
+      this.numbre = Math.floor(Math.random() * 9);
+      this.Alea = this.citationtext[this.numbre];
+    }, 2000);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: 0.2s opacity ease-out;
+}
+
+.fond {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.6);
+  z-index: 1;
+}
+.content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: calc(100% - 20px);
+  max-width: 500px;
+  transform: translate(-50%, -50%);
+  background: #fff;
+  border-radius: 3px;
+  padding: 20px;
+  line-height: 1.5;
+  font-size: 18px;
+  color: #444;
+  box-shadow: -2rem 2rem 2rem rgba(black, 0.2);
+}
+</style>
